e071fba2dfb016b17cfc564095824686e7d4ad97
Mikus Rakis <mikus.rakis@gmail.com>
2026-07-04 22:19:11 +0300

    remove imports

--- src/test/java/lv/bootcamp/shelter/task1/AgeCalculatorTest.java
+++ src/test/java/lv/bootcamp/shelter/task1/AgeCalculatorTest.java
@@ -3,8 +3,6 @@ package lv.bootcamp.shelter.task1;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
